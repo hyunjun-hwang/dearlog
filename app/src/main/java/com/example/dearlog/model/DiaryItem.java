d34d8d4259218d8a036cfc24f6ef497eb3b961b9
RyuDahyun <ekgus6801@naver.com>
2025-06-12 11:36:59 +0900

    감정통계 전 수정 작업

--- app/src/main/java/com/example/dearlog/model/DiaryItem.java
+++ app/src/main/java/com/example/dearlog/model/DiaryItem.java
@@ -1,16 +1,24 @@
 package com.example.dearlog.model;
 
 public class DiaryItem {
+    private int entryId;  // ✅ 추가
     private String date;
     private String question;
     private String writer;
     private String color;
 
-    public DiaryItem(String date, String question, String writer, String color) {
+    // 생성자 수정
+    public DiaryItem(int entryId, String date, String question, String writer, String color) {
+        this.entryId = entryId;
         this.date = date;
         this.question = question;
         this.writer = writer;
         this.color = color;
+    }
+
+    // getter 추가
+    public int getEntryId() {
+        return entryId;
     }
 
     public String getDate() {
